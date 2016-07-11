a55eb3907b326c4a53d7c6fcefc7c838b2fdbda4
indifferen7 <martin@casualsemantics.se>
2016-07-11 20:29:08 +0200

    Initial attempt

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -8,19 +8,18 @@
   char used as the start of the alphabet.
   Any leading characters are appended to
   the end of the resulting char array."
-  ([c] (flatten
-         (cons
-           (map char (range (int c) 123))
-           (map char (range 97 (int c)))))))
+  [c]
+  (flatten
+    (cons
+      (map char (range (int c) 123))
+      (map char (range 97 (int c))))))
 
 (defn char->index
   "Returns the index of char c in the
   provided colleciton of chars in xs."
   [c xs]
   (loop [current xs i 0]
-    (if (or
-          (empty? current)
-          (= (first current) c))
+    (if (= (first current) c)
       i
       (recur (rest current) (inc i)))))
 
@@ -36,6 +35,18 @@
   "Simply zips the two collections together."
   [xs ys] (map vector xs ys))
 
+(defn encode [keyword message]
+  (let [repeated-keyword (repeat-keyword keyword (count message))]
+    (apply str
+           (for [[c1 c2] (zip repeated-keyword message)]
+             (nth (shift-alphabet c1) (char->index c2 alphabet))))))
+
+(defn decode [keyword message]
+  (let [repeated-keyword (repeat-keyword keyword (count message))]
+    (apply str
+           (for [[c1 c2] (zip repeated-keyword message)]
+             (nth alphabet (char->index c2 (shift-alphabet c1)))))))
+
 (defn extract-keyword
   "Extracts the keyword based on the provided
   cipher and message."
@@ -50,29 +61,17 @@
   keyword. If candidate equals keyword, the function
   returns true."
   [candidate keyword]
-  (let [result (repeat-keyword candidate (count keyword))]
-    (= result keyword)))
-
-(defn encode [keyword message]
-  (let [repeated-keyword (repeat-keyword keyword (count message))]
-    (apply str
-           (for [[c1 c2] (zip repeated-keyword message)]
-             (nth (shift-alphabet c1) (char->index c2 alphabet))))))
-
-(defn decode [keyword message]
-  (let [repeated-keyword (repeat-keyword keyword (count message))]
-    (apply str
-           (for [[c1 c2] (zip repeated-keyword message)]
-             (nth alphabet (char->index c2 (shift-alphabet c1)))))))
+  (= (repeat-keyword candidate (count keyword)) keyword))
 
 (defn decipher [cipher message]
   (let [keyword (extract-keyword cipher message)]
     (loop [remaining keyword
            acc []]
       (let [candidate (apply str acc)]
-        (if (or
-              (empty? remaining)
-              (equals-repeated? candidate keyword))
+        (if
+          (or
+            (empty? remaining)
+            (equals-repeated? candidate keyword))
           candidate
           (recur (rest remaining) (conj acc (first remaining))))))))
 
